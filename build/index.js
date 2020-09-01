a62bbf1ee7c53fe884f739711bdbec36eb2d50e5
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-02 00:17:29 +0530

    removed build dir from git

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Container = void 0;
-const container_1 = require("./container");
-Object.defineProperty(exports, "Container", { enumerable: true, get: function () { return container_1.Container; } });
